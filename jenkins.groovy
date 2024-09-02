a3da43d734a6c5785967721789120ba2349a1da8
prada <rr>
2024-09-02 14:53:24 +0300

    1

---
+++ jenkins.groovy
@@ -0,0 +1,65 @@
+pipeline {
+    agent any
+
+    parameters {
+        booleanParam(name: 'Release_PSI', defaultValue: true, description: 'Scanning Sast & Oss')
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+            post {
+                success {
+                    echo 'Archiving...'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+
+        stage('Start SAST') {
+            when {
+                expression {
+                    branch 'master/*'
+                    params.Release_PSI == true
+                }
+            }
+            steps {
+                echo 'Start Sast'
+            }
+        }
+
+        stage('Start OSS') {
+            when {
+                expression {
+                    branch 'master/*'
+                    params.Release_PSI == true
+                }
+            }
+            steps {
+                echo 'Start Oss'
+            }
+        }
+
+//        stage('Sast & Oss') {
+//            when {
+//                expression {
+//                    branch 'master/*'
+//                    params.Release_PSI == true
+//                }
+//            }
+//
+//            steps {
+//                echo 'Start Sast & Oss_2'
+//            }
+//        }
+
+        stage('Deploy IFT') {
+            steps {
+                echo 'Deploy IFT'
+            }
+        }
+
+    }
+}
